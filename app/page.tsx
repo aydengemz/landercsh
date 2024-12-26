8b6df52deac88b7ecf5a7d4f13dbc993478f88b2
Ayden Xu <aydenxunc@gmail.com>
2024-12-25 21:54:02 -0500

    logoso

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ const MainContent = () => {
   const handleAffiliateClick = async () => {
     await new Promise((resolve) => setTimeout(resolve, 800));
     window.open(
-      "https://t.afftrackr.com/?yte=ctpq4NGRdY9H9EqNoDVAUY5bKl3pvaM5vQJDRoz7h5U%3d&s1=",
+      "https://t.afftrackr.com/?f5c=7oalGrDCjNbVrVkEg195FAH60J1024fIvQJDRoz7h5U%3d&s1=",
       "_blank"
     );
   };
