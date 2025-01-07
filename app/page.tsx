4a25816c1f6de5374246ec9a8ac35ddd9fdb0224
Ayden Xu <aydenxunc@gmail.com>
2025-01-07 10:21:20 -0500

    lowerid

--- app/page.tsx
+++ app/page.tsx
@@ -106,10 +106,7 @@ const MainContent = () => {
 
   const handleAffiliateClick = async () => {
     await new Promise((resolve) => setTimeout(resolve, 800));
-    window.open(
-      "https://glstrck.com/aff_c?offer_id=469&aff_id=11848",
-      "_blank"
-    );
+    window.location.href = "https://glstrck.com/aff_c?offer_id=469&aff_id=11848";
   };
 
   return (
