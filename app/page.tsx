0870c570d5fbb5464a0a69ac458bcb067da1038b
Ayden Xu <aydenxunc@gmail.com>
2025-01-29 10:57:33 -0500

    try

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ const MainContent = () => {
   }, []);
 
   const handleAffiliateClick = async () => {
-    window.location.href = "https://glstrck.com/aff_c?offer_id=1232&aff_id=11848&source=open";
+    window.location.href = "https://glstrck.com/aff_c?offer_id=1145&aff_id=11848&source=so";
   };
   
   
